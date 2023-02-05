2e0292c5e4e66f63c3fb21bee8c1a15bd99eea6d
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2023-02-05 13:30:24 +0100

    use input box and output channel

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import { ChatGPTAPI } from "chatgpt";
-import * as vscode from "vscode";
+import { window, commands, ExtensionContext } from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export async function activate(context: vscode.ExtensionContext) {
+export async function activate(context: ExtensionContext) {
   const api = new ChatGPTAPI({
     apiKey: process.env.OPENAI_API_KEY || "",
   });
@@ -15,19 +15,51 @@ export async function activate(context: vscode.ExtensionContext) {
     'Congratulations, your extension "chatgpt-pair-programming" is now active!'
   );
 
+  /**
+   * Shows a pick list using window.showQuickPick().
+   */
+  async function showQuickPick() {
+    let i = 0;
+    // const result = await window.showQuickPick(["Rust", "Go", "Brainfuck"], {
+    //   placeHolder: "eins, zwei or drei",
+    //   onDidSelectItem: (item) =>
+    //     window.showInformationMessage(`Focus ${++i}: ${item}`),
+    // });
+
+    const result = await window.showInputBox({
+      value: "python",
+      valueSelection: [0, -1],
+      placeHolder: "For example: Python, Scala, Node.",
+      validateInput: (text) => {
+        window.showInformationMessage(`Validating: ${text}`);
+        return text === "123" ? "Not 123!" : null;
+      },
+    });
+
+    if (result) {
+      console.log(`Selected ${result}, asking ChatGPT...`);
+      const ask = api.sendMessage(
+        `Write a Hello World! app in ${result}. And explain it.`
+      );
+      window.setStatusBarMessage("ChatGPT...", ask);
+      const res = await ask;
+      console.log(res);
+      window.showInformationMessage("ChatGPT replied");
+      const l = window.createOutputChannel("ChatGPT Pair Programming", {
+        log: true,
+      });
+      l.append(res.text);
+      l.show();
+    }
+  }
+
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
+  let disposable = commands.registerCommand(
     "chatgpt-pair-programming.helloWorld",
     async () => {
-      const res = await api.sendMessage("Hello World!");
-      console.log(res.text);
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from ChatGPT Pair Programming!"
-      );
+      showQuickPick();
     }
   );
 
